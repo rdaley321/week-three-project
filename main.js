45478c9ef626aabf02319a4bb932a977e5d0bf06
Ryan Daley <rdaley321@yahoo.com>
2017-06-12 18:16:00 -0500

    functionality

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const zero = document.querySelector('#zero')
 const one = document.querySelector('#one')
 const two = document.querySelector('#two')
 const three = document.querySelector('#three')
@@ -11,16 +12,37 @@ const clear = document.querySelector('#clear')
 const multiply = document.querySelector('#multiply')
 const divide = document.querySelector('#divide')
 const add = document.querySelector('#add')
-const subtract = document.querySelector('#subtract')
+const minus = document.querySelector('#minus')
 const decimal = document.querySelector('#decimal')
 const answer = document.querySelector('#answer')
-const all = [one,two,three,four,five,six,seven,eight,nine,multiply,divide,add,subtract,decimal]
-
-for(let i = 0; i < all.length; i++) {
-  let temp = []
-  if(all[i].addEventListener('click',function(op){
-    temp.push = all[i].textContent
-    console.log(temp)
+const equals = document.querySelector('#equals')
+const button = document.querySelectorAll('.button')
+const array = []
+const answerBox = []
+for(let i=0; i < button.length; i++) {
+  button[i].addEventListener('click', function(){
+    if(button[i] === zero || button[i] === one || button[i] === two || button[i] === three || button[i] === four || button[i] === five || button[i] === six || button[i] === seven || button[i] === eight || button[i] === nine || button[i] === decimal || button[i] === divide || button[i] === minus || button[i] === add) {
+    answerBox.push(button[i].textContent)
+    array.push(button[i].textContent)
+    answer.textContent = answerBox.join('')
+  } else if(button[i] === multiply) {
+    answerBox.push("x")
+    array.push("*")
+    answer.textContent = answerBox.join('')
+  } else if(button[i] === equals) {
+    const final = eval(array.join(''))
+    answer.textContent = final
+    array.length = 0
+    answerBox.length = 0
+    array.push(answer.textContent)
+    answerBox.push(answer.textContent)
+  } else if(button[i] === clear) {
+    array.length = 0
+    answerBox.length = 0
+    answer.textContent = ''
+    return array + answerBox
+  }
+    console.log("array", array)
+    console.log("answerBox", answerBox)
   })
-  return temp
 }
